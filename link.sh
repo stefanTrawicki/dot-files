8ce12475603f98002efd57a8c74e00ac1156a3a1
Stefan Trawicki <s.trawicki@me.com>
2022-04-30 17:47:03 +0100

    .

--- link.sh
+++ link.sh
@@ -1,9 +1,9 @@
-rm /home/$(whoami)/.zshrc
-rm /home/$(whoami)/.vimrc
-ln zshrc.txt /home/$(whoami)/.zshrc
-ln vimrc.txt /home/$(whoami)/.vimrc
+rm /home/$(whoami)/.zshrc &> /dev/null
+rm /home/$(whoami)/.vimrc &> /dev/null
+ln zshrc.txt /home/$(whoami)/.zshrc &> /dev/null
+ln vimrc.txt /home/$(whoami)/.vimrc &> /dev/null
 
-rm /Users/$(whoami)/.zshrc
-rm /Users/$(whoami)/.vimrc
-ln zshrc.txt /Users/$(whoami)/.zshrc
-ln vimrc.txt /Users/$(whoami)/.vimrc
+rm /Users/$(whoami)/.zshrc &> /dev/null
+rm /Users/$(whoami)/.vimrc &> /dev/null
+ln zshrc.txt /Users/$(whoami)/.zshrc &> /dev/null
+ln vimrc.txt /Users/$(whoami)/.vimrc &> /dev/null
